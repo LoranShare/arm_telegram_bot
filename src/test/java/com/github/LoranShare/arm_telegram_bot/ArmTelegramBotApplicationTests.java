02ca79acb09544075976d69f737a9765f87bb4b4
LoranSharan <useworkstudy@hotmail.com>
2022-09-02 19:43:50 +0300

    Create Spring boot project

---
+++ src/test/java/com/github/LoranShare/arm_telegram_bot/ArmTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.LoranShare.arm_telegram_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
